b803c5853fa818475ab5b5b5d654c4d84c667571
d02ev <vikramadityapratapsingh98@gmail.com>
2022-09-04 13:17:37 +0530

    Update LogSchema.js

--- models/LogSchema.js
+++ models/LogSchema.js
@@ -13,10 +13,20 @@ const log_schema = new Mongoose.Schema({
         type: Number,
         required: true
     },
-    log: {
-        type: Array,
-        required: true
-    }
+    log: [{
+          description: {
+            type: String,
+            required: true
+          },
+          duration: {
+            type: String,
+            required: true
+          },
+          date: {
+            type: String,
+            required: true
+          }
+    }]
 },
     {
         collection: 'logs'
